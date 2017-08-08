9c0663f95a0750a39a468e25e3b5ae6df26e9776
Zack Beach <11981152+zbeach@users.noreply.github.com>
2017-08-08 16:35:16 -0400

    Write eachToString method for each primitive type

--- src/main/java/zb_utils/Arrays.java
+++ src/main/java/zb_utils/Arrays.java
@@ -4,15 +4,80 @@ public class Arrays {
 	/**
 	 * Makes a String array of the String representation of each element in an
 	 * array of objects.
+	 * @param <T>
 	 * @param data An array of objects
 	 * @return String array of the String representation of each element in the
 	 * array of objects
 	 */
-	public static String[] eachToString(Object[] data) {
+	public static <T> String[] eachToString(T[] data) {
 		String[] dataStrings = new String[data.length];
 		
 		for (int i = 0; i < data.length; i++)
 			dataStrings[i] = data[i].toString();
+		
+		return dataStrings;
+	}
+	
+	/**
+	 * Makes a String array of the String representation of each element in an
+	 * array of booleans.
+	 * @param data An array of booleans
+	 * @return String array of the String representation of each element in the
+	 * array of booleans
+	 */
+	public static String[] eachToString(boolean[] data) {
+		String[] dataStrings = new String[data.length];
+		
+		for (int i = 0; i < data.length; i++)
+			dataStrings[i] = "" + data[i];
+		
+		return dataStrings;
+	}
+	
+	/**
+	 * Makes a String array of the String representation of each element in an
+	 * array of bytes.
+	 * @param data An array of bytes
+	 * @return String array of the String representation of each element in the
+	 * array of bytes
+	 */
+	public static String[] eachToString(byte[] data) {
+		String[] dataStrings = new String[data.length];
+		
+		for (int i = 0; i < data.length; i++)
+			dataStrings[i] = "" + data[i];
+		
+		return dataStrings;
+	}
+	
+	/**
+	 * Makes a String array of the String representation of each element in an
+	 * array of chars.
+	 * @param data An array of chars
+	 * @return String array of the String representation of each element in the
+	 * array of chars
+	 */
+	public static String[] eachToString(char[] data) {
+		String[] dataStrings = new String[data.length];
+		
+		for (int i = 0; i < data.length; i++)
+			dataStrings[i] = "" + data[i];
+		
+		return dataStrings;
+	}
+	
+	/**
+	 * Makes a String array of the String representation of each element in an
+	 * array of shorts.
+	 * @param data An array of shorts
+	 * @return String array of the String representation of each element in the
+	 * array of shorts
+	 */
+	public static String[] eachToString(short[] data) {
+		String[] dataStrings = new String[data.length];
+		
+		for (int i = 0; i < data.length; i++)
+			dataStrings[i] = "" + data[i];
 		
 		return dataStrings;
 	}
@@ -25,6 +90,54 @@ public class Arrays {
 	 * array of ints
 	 */
 	public static String[] eachToString(int[] data) {
+		String[] dataStrings = new String[data.length];
+		
+		for (int i = 0; i < data.length; i++)
+			dataStrings[i] = "" + data[i];
+		
+		return dataStrings;
+	}
+	
+	/**
+	 * Makes a String array of the String representation of each element in an
+	 * array of longs.
+	 * @param data An array of longs
+	 * @return String array of the String representation of each element in the
+	 * array of longs
+	 */
+	public static String[] eachToString(long[] data) {
+		String[] dataStrings = new String[data.length];
+		
+		for (int i = 0; i < data.length; i++)
+			dataStrings[i] = "" + data[i];
+		
+		return dataStrings;
+	}
+	
+	/**
+	 * Makes a String array of the String representation of each element in an
+	 * array of floats.
+	 * @param data An array of floats
+	 * @return String array of the String representation of each element in the
+	 * array of floats
+	 */
+	public static String[] eachToString(float[] data) {
+		String[] dataStrings = new String[data.length];
+		
+		for (int i = 0; i < data.length; i++)
+			dataStrings[i] = "" + data[i];
+		
+		return dataStrings;
+	}
+	
+	/**
+	 * Makes a String array of the String representation of each element in an
+	 * array of doubles.
+	 * @param data An array of doubls
+	 * @return String array of the String representation of each element in the
+	 * array of doubles
+	 */
+	public static String[] eachToString(double[] data) {
 		String[] dataStrings = new String[data.length];
 		
 		for (int i = 0; i < data.length; i++)
--- src/test/java/ArraysTest.java
+++ src/test/java/ArraysTest.java
@@ -8,7 +8,7 @@ import zb_utils.Arrays;
 
 public class ArraysTest {
 	@Test
-	public void testEachToString_Object_array() {
+	public void testEachToString_T_array() {
 		class Date {
 			private int day, month, year;
 			
@@ -37,15 +37,85 @@ public class ArraysTest {
 				zb_utils.Arrays.eachToString(dates),
 				dateStrings));
 	}
+
+	@Test
+	public void testEachToString_boolean_array() {
+		boolean[] bits = {false, true, true, false, true};
+		
+		String[] bitStrings = {"false", "true", "true", "false", "true"};
+		
+		assert(java.util.Arrays.equals(
+				zb_utils.Arrays.eachToString(bits), bitStrings));
+	}
+	
+	@Test
+	public void testEachToString_byte_array() {
+		byte[] bytes = {-128, 127, -127, 126, -126, 125};
+		
+		String[] byteStrings = {"-128", "127", "-127", "126", "-126", "125"};
+		
+		assert(java.util.Arrays.equals(
+				zb_utils.Arrays.eachToString(bytes), byteStrings));
+	}
+	
+	@Test
+	public void testEachToString_char_array() {
+		char[] chars = {'a', 'B', 'z', 'Y'};
+		
+		String[] charStrings = {"a", "B", "z", "Y"};
+		
+		assert(java.util.Arrays.equals(
+				zb_utils.Arrays.eachToString(chars), charStrings));
+	}
+	
+	@Test
+	public void testEachToString_short_array() {
+		short[] shorts = {-32768, 32767, -32767, 32766};
+		
+		String[] shortStrings = {"-32768", "32767", "-32767", "32766"};
+		
+		assert(java.util.Arrays.equals(
+				zb_utils.Arrays.eachToString(shorts), shortStrings));
+	}
 	
 	@Test
 	public void testEachToString_int_array() {
-		int[] nums = {0, 1, 2, 3, 4, 5};
+		int[] ints = {Integer.MIN_VALUE, Integer.MAX_VALUE, Integer.MIN_VALUE + 1, Integer.MAX_VALUE - 1};
 		
-		String[] numStrings = {"0", "1", "2", "3", "4", "5"};
+		String[] intStrings = {Integer.toString(Integer.MIN_VALUE), Integer.toString(Integer.MAX_VALUE), Integer.toString(Integer.MIN_VALUE + 1), Integer.toString(Integer.MAX_VALUE - 1)};
 		
 		assert(java.util.Arrays.equals(
-				zb_utils.Arrays.eachToString(nums), numStrings));
+				zb_utils.Arrays.eachToString(ints), intStrings));
+	}
+	
+	@Test
+	public void testEachToString_long_array() {
+		long[] longs = {Long.MIN_VALUE, Long.MAX_VALUE, Long.MIN_VALUE + 1, Long.MAX_VALUE - 1};
+		
+		String[] longStrings = {Long.toString(Long.MIN_VALUE), Long.toString(Long.MAX_VALUE), Long.toString(Long.MIN_VALUE + 1), Long.toString(Long.MAX_VALUE - 1)};
+		
+		assert(java.util.Arrays.equals(
+				zb_utils.Arrays.eachToString(longs), longStrings));
+	}
+	
+	@Test
+	public void testEachToString_float_array() {
+		float[] floats = {Float.MIN_VALUE, Float.MAX_VALUE, Float.MIN_VALUE + 1, Float.MAX_VALUE - 1};
+		
+		String[] floatStrings = {Float.toString(Float.MIN_VALUE), Float.toString(Float.MAX_VALUE), Float.toString(Float.MIN_VALUE + 1), Float.toString(Float.MAX_VALUE - 1)};
+		
+		assert(java.util.Arrays.equals(
+				zb_utils.Arrays.eachToString(floats), floatStrings));
+	}
+	
+	@Test
+	public void testEachToString_double_array() {
+		double[] doubles = {Double.MIN_VALUE, Double.MAX_VALUE, Double.MIN_VALUE + 1, Double.MAX_VALUE - 1};
+		
+		String[] doubleStrings = {Double.toString(Double.MIN_VALUE), Double.toString(Double.MAX_VALUE), Double.toString(Double.MIN_VALUE + 1), Double.toString(Double.MAX_VALUE - 1)};
+	
+		assert(java.util.Arrays.equals(
+				zb_utils.Arrays.eachToString(doubles), doubleStrings));
 	}
 	
 	@Test
